799f9c64fbe5ec120c0520e174cccd8657b097cd
FidanXankishiyeva <fidan.xankisiyev06@gmail.com>
2023-10-31 13:32:38 +0400

    add js faq page

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let aTag=document.createElement('a')
 aTag.innerText = 'Read More'
 div.append (aTag)
 
+let collajDiv=document.querySelector('.card-box')
+collajDiv.style.background = '#F9B572'
+
+
+let text = document.querySelector('.h4Text')
+text.innerText='Best marketing tools'
+
+function number (a){
+    a++
+    console.log(a);
+}
+number(5)
+number(9)
